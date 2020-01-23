3c21a25ef9f26c1353f7eeaaaa62a45ce2b380dd
Qin Guan <qinguan20040914@gmail.com>
2020-01-23 02:01:20 +0000

    added pages

--- meth/views.py
+++ meth/views.py
@@ -1,5 +1,7 @@
+# Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("you succ")
+    template = loader.get_template("meth.html")
+    return HttpResponse(template.render())
