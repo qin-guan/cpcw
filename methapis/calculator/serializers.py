a04063f8103720c5d5a9549c26cf6e27ab236418
qinguan <qinguan20040914@gmail.com>
2020-03-04 09:09:44 +0800

    added url validation

--- methapis/calculator/serializers.py
+++ methapis/calculator/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Calculator
+import re
 
 class CalculatorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,8 +24,20 @@ class CalculatorSerializer(serializers.ModelSerializer):
          )
 
     def validate(self, data):
+        regex = re.compile(
+        r'^(?:http|ftp)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?' 
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
         mapping = data['calculation_formula_var_mapping']
         graph_formula = data['graph_formula']
+        resource_links = data["resource_links"]
+        if len(resource_links) > 0 and resource_links != "_":
+            for i in resource_links.splitlinea():
+                if re.match(regex, i) == False: 
+                    raise serializers.ValidationError("Must be valid link")
         if len(graph_formula) > 0 and graph_formula != "_":
             for i in graph_formula.split(","):
                 if not len(i.split('=')) == 2:
