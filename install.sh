34460305260a957761b4eabc482c5d5f594e4f15
qinguan <qinguan20040914@gmail.com>
2020-03-05 10:49:20 +0800

    sh

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@ pip3 install Django;
 pip3 install djangorestframework;
 pip3 install django-cors-headers;
 pip3 install django-health-check;
-
-cd methdealer;
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash;
 nvm ls;
 nvm install node;
 nvm use node;
-
+cd methdealer;
+npm i;
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 cd methapis;
 python3 manage.py runserver 0:5061;
 cd ../methdealer;
-npm i;
 npm run build;
 npm run start;
