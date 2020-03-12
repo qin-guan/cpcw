3e134b4627457e7276268e62d393f10e84446dc9
Qin Guan <qinguan20040914@gmail.com>
2020-03-12 13:33:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from subprocess import Popen
+import webbrowser
 commands = [['sh', 'runPython.sh'], ['sh', 'runUI.sh']]
 procs = [ Popen(i) for i in commands ]
 for p in procs:
    p.wait()
+
+webbrowser.open("http://localhost:3000/")
